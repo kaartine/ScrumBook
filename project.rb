af41f36d37f40d3ebf2ac8bbd7637d4cad296e45
Jukka Kaartinen <jkaartinen@gmail.com>
2011-01-10 23:45:26 +0200

    tasks changed from array to hash
    Signed-off-by: Jukka Kaartinen <jkaartinen@gmail.com>

--- project.rb
+++ project.rb
@@ -2,49 +2,62 @@ require './helpfunctions.rb'
 
 class Project
 
-	attr_accessor :name, :sprint, :sprintlength, :members, :tasks, :not_saved
+  attr_accessor :name, :sprint, :sprintlength, :members, :tasks, :not_saved, :fileName
 
-	def initialize
-		@sprintlength = 10
-		@not_saved = false
+  def initialize
+    @fileName = ""
+    @sprintlength = 10
+    @not_saved = false
 
-		@tasks = Array.new
+    @tasks = Hash.new
 
-		@tasks[0] = Array.new
-		@tasks[0] << Task.new("scrumbook 0", "JK", "not started")
-		@tasks[0] << Task.new("dcc 0", "GK", "in progress")
+#    @tasks[0] = Array.new
+#    @tasks[0] << Task.new("scrumbook 0", "JK", "not started")
+#    @tasks[0] << Task.new("dcc 0", "GK", "in progress")
 
-		@tasks << Array.new
-		@tasks[1] << Task.new("scrumbook 1", "JK", "not started")
-		@tasks[1] << Task.new("dcc 1", "GK", "in progress")
+#    @tasks << Array.new
+#    @tasks[1] << Task.new("scrumbook 1", "JK", "not started")
+#    @tasks[1] << Task.new("dcc 1", "GK", "in progress")
+  end
 
-		logger @tasks.to_s
-	end
+  def saved?
+    @not_saved
+  end
 
-	def saved?
-		@not_saved
-	end
+  def getActiveSprintsTasks
+    @tasks[@sprint]
+  end
 
-	def getActiveSprintsTasks
-		@tasks[@sprint]
-	end
+  def addNewTaskToSprint(newTask)
+    logger "before: " + @tasks.inspect
+    if !@tasks.has_key?(@sprint)
+      @tasks[@sprint] = Array.new
+    end
+
+    if @tasks[@sprint].index(newTask.name).nil?
+      @tasks[@sprint].push(newTask)
+    else
+      nil
+    end
+
+  end
 
 end
 
 
 class Task
-	attr_accessor :committer, :status, :duration, :name
+  attr_accessor :committer, :status, :duration, :name
 
-	def initialize( name, committer, status )
-		@name = name
-		@committer = committer
-		@status = status
-		@duration = Array.new
-		@duration.push(0)
-	end
+  def initialize( name, committer, status )
+    @name = name
+    @committer = committer
+    @status = status
+    @duration = Array.new
+    @duration.push("")
+  end
 
-	def ==(id)
-		self.name == id
-	end
+  def ==(id)
+    self.name == id
+  end
 
 end
--- scrumbook.rb
+++ scrumbook.rb
@@ -10,8 +10,8 @@ require './helpfunctions.rb'
 class ScrumBook
 
   def initialize
-  	@days = ['ma', 'ti', 'ke', 'to', 'pe']
-  	@project = Project.new
+    @days = ['ma', 'ti', 'ke', 'to', 'pe']
+    @project = Project.new
 
     @root = TkRoot.new
     @root.title = "ScrumBook"
@@ -22,7 +22,7 @@ class ScrumBook
 
   def createTabs
     tab = Tk::Tile::Notebook.new(@root) do
-    	width 800
+      width 1000
     end
 
     createConfigTab(tab)
@@ -38,193 +38,223 @@ class ScrumBook
   end
 
   def refreshSprint(selected_item = nil)
-  	id = @projectSprint.value.to_i
-  	logger "sprint_id: " + id.to_s
-  	@project.sprint = id
+    id = @projectSprint.value.to_i
+    logger "sprint_id: " + id.to_s
+    @project.sprint = id
 
-		items = @sprintTaskTree.children('')
-  	@sprintTaskTree.delete(items)
+    items = @sprintTaskTree.children('')
+    @sprintTaskTree.delete(items)
 
-		if @project.tasks[id]
-	  	@project.tasks[id].each do |t|
-	  		@sprintTaskTree.insert('', 'end', :id => t.name, :text => t.name, :tags => ['clickapple'])
-	  		@sprintTaskTree.set( t.name, 'committer', t.committer)
-	  		@sprintTaskTree.set( t.name, 'status', t.status)
+    if @project.tasks[id]
+      @project.tasks[id].each do |t|
+        @sprintTaskTree.insert('', 'end', :id => t.name, :text => t.name, :tags => ['clickapple'])
+        @sprintTaskTree.set( t.name, 'committer', t.committer)
+        @sprintTaskTree.set( t.name, 'status', t.status)
 
-	  		@project.sprintlength.to_i.times.each do |i|
-					@sprintTaskTree.set(t.name, "w#{i}", t.duration[i])
-				end
+        @project.sprintlength.to_i.times.each do |i|
+          @sprintTaskTree.set(t.name, "w#{i}", t.duration[i])
+        end
 
- 				@sprintTaskTree.tag_bind('clickapple', 'ButtonRelease-1', @changeTask);
-	  	end
-	  	@sprintTaskTree.selection_set(selected_item) if !selected_item.nil?
+         @sprintTaskTree.tag_bind('clickapple', 'ButtonRelease-1', @changeTask);
+      end
+      @sprintTaskTree.selection_set(selected_item) if !selected_item.nil?
 
-	  	logger "project: " + @project.inspect
-	  end
+      logger "project: " + @project.inspect
+    end
 
-#		@sprintTaskTree.insert('', 'end', :id => 'widgets', :text => 'Widget Tour')
-#		@sprintTaskTree.insert('', 0, :id => 'gallery', :text => 'Applications')
-#		item = @sprintTaskTree.insert('', 'end', :text => 'Tutorial')
-#		@sprintTaskTree.insert( 'widgets', 'end', :text => 'Canvas')
-#		@sprintTaskTree.insert( item, 'end', :text => 'Tree')
-#		@sprintTaskTree.set('widgets', 'committer', 'JK'); # or item.set('size', '12KB')
-#		size = @sprintTaskTree.get('widgets', 'committer');  # or item.get('size')
-#		@sprintTaskTree.insert('', 'end', :text => 'Listbox', :values => ['GK','Done','0'])
+#    @sprintTaskTree.insert('', 'end', :id => 'widgets', :text => 'Widget Tour')
+#    @sprintTaskTree.insert('', 0, :id => 'gallery', :text => 'Applications')
+#    item = @sprintTaskTree.insert('', 'end', :text => 'Tutorial')
+#    @sprintTaskTree.insert( 'widgets', 'end', :text => 'Canvas')
+#    @sprintTaskTree.insert( item, 'end', :text => 'Tree')
+#    @sprintTaskTree.set('widgets', 'committer', 'JK'); # or item.set('size', '12KB')
+#    size = @sprintTaskTree.get('widgets', 'committer');  # or item.get('size')
+#    @sprintTaskTree.insert('', 'end', :text => 'Listbox', :values => ['GK','Done','0'])
 
-#		@sprintTaskTree.itemconfigure('widgets', 'open', true); # or item['open'] = true
-#		isopen = @sprintTaskTree.itemcget('widgets', 'open');   # or isopen = item['open']
+#    @sprintTaskTree.itemconfigure('widgets', 'open', true); # or item['open'] = true
+#    isopen = @sprintTaskTree.itemcget('widgets', 'open');   # or isopen = item['open']
 
   end
 
   def refreshTaskEditor
-  	item = @sprintTaskTree.focus_item()
-		logger "selected: " + item.inspect
+    item = @sprintTaskTree.focus_item()
+    logger "selected: " + item.inspect
 
-		if !item.nil?
-			tasks = @project.getActiveSprintsTasks()
-			id = tasks.index(item.id)
+    if !item.nil?
+      tasks = @project.getActiveSprintsTasks()
+      id = item.to_s.to_i
 
-			@taskName.value = tasks[id].name
-			@taskCommitter.value = tasks[id].committer
-			@taskStatus.value = tasks[id].status
-			@project.sprintlength.to_i.times.each  do |i|
-				@taskDuration[i].value = tasks[id].duration[i]
-			end
-		end
-	end
+      logger "task id: " + id.to_s
 
-	def updateTask(id)
-		logger "updateTask: " + id.to_s
+      @taskName.value = tasks[id].name
+      @taskCommitter.value = tasks[id].committer
+      @taskStatus.value = tasks[id].status
+      @project.sprintlength.to_i.times.each  do |i|
+        @taskDuration[i].value = tasks[id].duration[i]
+      end
+    end
+  end
 
-		tasks = @project.getActiveSprintsTasks()
-		id = tasks.index(id)
-		logger "task id: " + id.to_s
+  def updateTask(id)
+    logger "updateTask: " + id.to_s
 
-		if !id.nil?
-			tasks[id].name = @taskName.value
-			tasks[id].committer = @taskCommitter.value
-			tasks[id].status = @taskStatus.value
-			@project.sprintlength.to_i.times.each  do |i|
-				tasks[id].duration[i] = @taskDuration[i].value
-				logger "task update w#{i}: " + tasks[id].duration[i]
-			end
+    tasks = @project.getActiveSprintsTasks()
+#    id = tasks.index(id)
+    logger "task id: " + id.to_s
 
-			@project.not_saved = true
-		end
+    id = id.to_i
 
-		refreshSprint(tasks[id].name)
-	end
+    if !id.nil?
+      tasks[id].name = @taskName.value
+      tasks[id].committer = @taskCommitter.value
+      tasks[id].status = @taskStatus.value
+      @project.sprintlength.to_i.times.each  do |i|
+        tasks[id].duration[i] = @taskDuration[i].value
+        logger "task update w#{i}: " + tasks[id].duration[i]
+      end
 
-	def procUpdateTask
-		item = @sprintTaskTree.focus_item()
-		updateTask(@sprintTaskTree.focus_item().id) if !item.nil?
-	end
+      @project.not_saved = true
+    end
 
-	def createSprintTab(tab)
-		@sprintTab = TkFrame.new(tab)
+    refreshSprint(tasks[id].name)
+  end
 
-		@changeSprint = Proc.new {
-			refreshSprint
-		}
+  def procUpdateTask
+    item = @sprintTaskTree.focus_item()
+    updateTask(@sprintTaskTree.focus_item().id) if !item.nil?
+  end
 
-		@changeTask = Proc.new {
-			refreshTaskEditor
-		}
+  def procAddNewTask
+    task = Task.new(@taskName.value, @taskCommitter.value, @taskStatus.value)
+    @project.sprintlength.to_i.times.each  do |i|
+      task.duration[i] = @taskDuration[i].value
+      logger "task update w#{i}: " + task.duration[i]
+    end
+    if @project.addNewTaskToSprint(task).nil?
+      Tk.messageBox(
+        'type'    => "ok",
+        'icon'    => "info",
+        'title'   => "Title",
+        'message' => "Task #{@taskName.value} already exists!"
+      )
+    end
+    refreshView
+  end
 
-		#sprint selector
-		sprintEntry = TkSpinbox.new(@sprintTab) do
-			to 1000
-			from 0
-			increment 1
-			width 10
-			command {$s.refreshSprint}
-		end
 
-		sprintEntry.bind("ButtonRelease-1", @changeSprint)
+  def createSprintTab(tab)
+    @sprintTab = TkFrame.new(tab)
 
-		@projectSprint = TkVariable.new
-		@projectSprint.value = "1"
-		sprintEntry.textvariable = @projectSprint
-		sprintEntry.pack("side" => "left")
+    @changeSprint = Proc.new {
+      refreshSprint
+    }
 
-		@sprintTaskTree = Tk::Tile::Treeview.new(@sprintTab)
+    @changeTask = Proc.new {
+      refreshTaskEditor
+    }
 
-		columns = 'committer status'
-		@project.sprintlength.to_i.times.each do |d|
-			columns += " w#{d}"
-		end
+    #sprint selector
+    sprintEntry = TkSpinbox.new(@sprintTab) do
+      to 1000
+      from 0
+      increment 1
+      width 10
+      command {$s.refreshSprint}
+    end
 
-		logger columns
-	  @sprintTaskTree['columns'] = columns.to_s
-		logger @sprintTaskTree['columns'], 4
+    sprintEntry.bind("ButtonRelease-1", @changeSprint)
 
-		@sprintTaskTree.column_configure( 'committer', :width => 90, :anchor => 'center')
-		@sprintTaskTree.heading_configure( 'committer', :text => 'Committer')
-		@sprintTaskTree.heading_configure( 'status', :text => 'Status')
+    @projectSprint = TkVariable.new
+    @projectSprint.value = "1"
+    sprintEntry.textvariable = @projectSprint
+    sprintEntry.pack("side" => "left")
 
-		i = 0
-		@project.sprintlength.to_i.times.each do |d|
-			@sprintTaskTree.heading_configure( "w#{d}", :text => @days[i])
-			@sprintTaskTree.column_configure( "w#{d}", :width => 10, :anchor => 'center')
-			i+=1
-			if i >= 5
-			 	i = 0
-			end
-			logger i, 4
-		end
+    @sprintTaskTree = Tk::Tile::Treeview.new(@sprintTab)
 
-		@sprintTaskTree.pack("side" => "top")
+    columns = 'committer status'
+    @project.sprintlength.to_i.times.each do |d|
+      columns += " w#{d}"
+    end
 
-		refreshSprint
+    logger columns
+    @sprintTaskTree['columns'] = columns.to_s
+    logger @sprintTaskTree['columns'], 4
 
-		# Task edition fields
-		@taskName = TkVariable.new
-		@taskCommitter = TkVariable.new
-		@taskStatus = TkVariable.new
-		@taskDuration = Array.new
-		@project.sprintlength.to_i.times.each  do |i|
-			@taskDuration << TkVariable.new
-		end
+    @sprintTaskTree.column_configure( 'committer', :width => 90, :anchor => 'center')
+    @sprintTaskTree.heading_configure( 'committer', :text => 'Committer')
+    @sprintTaskTree.heading_configure( 'status', :text => 'Status')
 
-		taskNameEntry = TkEntry.new(@sprintTab)
-		taskNameEntry.textvariable = @taskName
-		taskNameEntry.pack("side" => "left")
+    i = 0
+    @project.sprintlength.to_i.times.each do |d|
+      @sprintTaskTree.heading_configure( "w#{d}", :text => @days[i])
+      @sprintTaskTree.column_configure( "w#{d}", :width => 10, :anchor => 'center')
+      i+=1
+      if i >= 5
+         i = 0
+      end
+      logger i, 4
+    end
 
-		taskCommitter = TkEntry.new(@sprintTab) #Tk::Tile::ComboBox.new(@sprintTab)
-		taskCommitter.textvariable = @taskCommitter
-		taskCommitter.pack("side" => "left")
+    @sprintTaskTree.pack("side" => "top")
 
-		taskStatus = TkEntry.new(@sprintTab) #Tk::Tile::ComboBox.new(@sprintTab)
-		taskStatus.textvariable = @taskStatus
-		taskStatus.pack("side" => "left")
+    refreshSprint
 
-		taskDurationEntry = Array.new
-		@project.sprintlength.to_i.times.each do |i|
-			taskDurationEntry << TkEntry.new(@sprintTab) do
-				width 3
-			end
-			taskDurationEntry[i].textvariable = @taskDuration[i]
-			taskDurationEntry[i].pack("side" => "left")
-		end
+    # Task edition fields
+    @taskName = TkVariable.new
+    @taskCommitter = TkVariable.new
+    @taskStatus = TkVariable.new
+    @taskDuration = Array.new
+    @project.sprintlength.to_i.times.each  do |i|
+      @taskDuration << TkVariable.new
+    end
 
-		# Task update button
-		TkButton.new(@sprintTab) do
-			text 'Update Task'
-			command( proc {$s.procUpdateTask})
-			pack "side" => "left"
-		end
+    taskNameEntry = TkEntry.new(@sprintTab)
+    taskNameEntry.textvariable = @taskName
+    taskNameEntry.pack("side" => "left")
 
-	end
+    taskCommitter = TkEntry.new(@sprintTab) #Tk::Tile::ComboBox.new(@sprintTab)
+    taskCommitter.textvariable = @taskCommitter
+    taskCommitter.pack("side" => "left")
+
+    taskStatus = TkEntry.new(@sprintTab) #Tk::Tile::ComboBox.new(@sprintTab)
+    taskStatus.textvariable = @taskStatus
+    taskStatus.pack("side" => "left")
+
+    taskDurationEntry = Array.new
+    @project.sprintlength.to_i.times.each do |i|
+      taskDurationEntry << TkEntry.new(@sprintTab) do
+        width 3
+      end
+      taskDurationEntry[i].textvariable = @taskDuration[i]
+      taskDurationEntry[i].pack("side" => "left")
+    end
+
+    # Task update button
+    TkButton.new(@sprintTab) do
+      text 'Update Task'
+      command( proc {$s.procUpdateTask})
+      pack "side" => "left"
+    end
+
+    # Add new task button
+    TkButton.new(@sprintTab) do
+      text 'Add new Task'
+      command( proc {$s.procAddNewTask})
+      pack "side" => "left"
+    end
+
+
+  end
 
   def createConfigTab(tab)
-		@configsTab = TkFrame.new(tab)
+    @configsTab = TkFrame.new(tab)
 
-		#project name
-		nameEntry = TkEntry.new(@configsTab)
-		@projectName = TkVariable.new
-		@projectName.value = "Enter Project's name"
-		nameEntry.textvariable = @projectName
-		nameEntry.place('height' => 25,
+    #project name
+    nameEntry = TkEntry.new(@configsTab)
+    @projectName = TkVariable.new
+    @projectName.value = "Enter Project's name"
+    nameEntry.textvariable = @projectName
+    nameEntry.place('height' => 25,
             'width'  => 150,
             'x'      => 10,
             'y'      => 10)
@@ -241,31 +271,40 @@ class ScrumBook
     }
 
     @save_click = Proc.new {
-    	saveProject
+      if @project.fileName.size > 0
+        saveProject
+      else
+        saveAsProject
+      end
     }
 
+    @saveAs_click = Proc.new {
+      saveAsProject
+    }
+
+
     @open_click = Proc.new {
-    	loadProject
+      loadProject
     }
 
 
 
     @exit = Proc.new {
-    	if @project.saved?
-      	exit
+      if @project.saved?
+        exit
       else
-     		 yes = Tk.messageBox(
-	        'type'    => "yesno",
-	        'icon'    => "question",
-	        'title'   => "Title",
-	        'message' => "Project is not saved! Save before exiting?",
-	        'default' => "yes"
-      		)
-      	if yes == "yes"
-      		saveProject
-      	end
+          yes = Tk.messageBox(
+          'type'    => "yesno",
+          'icon'    => "question",
+          'title'   => "Title",
+          'message' => "Project is not saved! Save before exiting?",
+          'default' => "yes"
+          )
+        if yes == "yes"
+          saveProject
+        end
 
-      	exit
+        exit
       end
     }
 
@@ -291,7 +330,7 @@ class ScrumBook
                   'underline' => 0)
     file_menu.add('command',
                   'label'     => "Save As...",
-                  'command'   => @menu_click,
+                  'command'   => @saveAs_click,
                   'underline' => 5)
     file_menu.add('separator')
     file_menu.add('command',
@@ -308,33 +347,41 @@ class ScrumBook
 
   end
 
+  def saveAsProject
+    fileName = Tk.getSaveFile
+    @project.fileName=fileName
+    logger "SaveAs fileName:" + fileName
+    saveProject
+  end
+
   def saveProject
-  	file = File.new "dumpedExample", 'w'
-  	@project.not_saved = true
-  	logger @project.inspect
-		serial = Marshal.dump( @project )
-		logger "serial: " + serial.inspect
-		file.write serial
-		file.close
-	end
+    file = File.new @project.fileName, 'w'
+    @project.not_saved = true
+    logger @project.inspect
+    serial = Marshal.dump( @project )
+    logger "serial: " + serial.inspect, 4
+    file.write serial
+    file.close
+  end
 
-	def loadProject
-		file = File.new 'dumpedExample', 'r'
-		serial = file.read
-		file.close
-		@project = Marshal.load( serial )
-		logger @project.inspect
+  def loadProject
+    fileName = Tk.getOpenFile
+    file = File.new fileName, 'r'
+    serial = file.read
+    file.close
+    @project = Marshal.load( serial )
+    logger @project.inspect
 
-		logger "serial: " + serial.inspect
+    logger "serial: " + serial.inspect, 4
 
-		refreshView
-	end
+    refreshView
+  end
 
 
-	def refreshView
-		refreshSprint
-		refreshTaskEditor
-	end
+  def refreshView
+    refreshSprint
+    refreshTaskEditor
+  end
 end
 
 $s = ScrumBook.new
